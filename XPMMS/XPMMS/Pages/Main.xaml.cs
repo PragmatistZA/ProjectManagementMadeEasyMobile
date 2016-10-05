daa778d475756345796799458add50e2a4e90b1e
PragmatistZA <shaun.cilliers@gmail.com>
2016-10-05 18:40:13 +0200

    Team view populates properly

--- XPMMS/XPMMS/Pages/Main.xaml.cs
+++ XPMMS/XPMMS/Pages/Main.xaml.cs
@@ -17,14 +17,14 @@ namespace XPMMS
 {
     public partial class Main : ContentPage
     {
-        private UserModel loggedInUser;
+        private UserModel _loggedInUser;
         public Main()
         {
             InitializeComponent();
             // hard coded user login data
             var jsonUserData = WebService.GetUser("sheldor@gmail.com");
             var users = JsonConvert.DeserializeObject<UserModel[]>(jsonUserData, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            loggedInUser = users[0];
+            _loggedInUser = users[0];
             setPage();
         }
 
@@ -124,9 +124,32 @@ namespace XPMMS
         }
 
         private async void BtnTeam_Clicked(object sender, EventArgs e)
-        {
+        {   
+            var jsonTeamContent = WebService.GetTeam(Convert.ToString(_loggedInUser.Team_ID));
+            var teamData = JsonConvert.DeserializeObject<TeamModel[]>(jsonTeamContent, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            TeamModel team = teamData[0];
 
-            await Navigation.PushAsync(new Team());
+            var jsonProjectData = WebService.GetTeamProjects(Convert.ToString(team.Team_Name));
+            var projectData = JsonConvert.DeserializeObject<ProjectModel[]>(jsonProjectData, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            ProjectModel project = projectData[0];
+
+            var jsonTasksData = WebService.GetAllTasks();
+            var tasks = JsonConvert.DeserializeObject<TaskModel[]>(jsonTasksData, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            List<TaskModel> taskList = new List<TaskModel>();
+
+            foreach (var task in tasks)
+            {
+                if (task.Project_ID == team.Proj_ID)
+                    taskList.Add(task);
+            }
+            tasks = taskList.ToArray();
+
+
+            var jsonMembersData = WebService.GetTeamMembers(Convert.ToString(team.Team_ID));
+            var members = JsonConvert.DeserializeObject<UserModel[]>(jsonMembersData, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            await Navigation.PushAsync(new Team(_loggedInUser, members, team, project, tasks));
         }
 
         private async void BtnProject_Clicked(object sender, EventArgs e)
--- XPMMS/XPMMS/Pages/Team.xaml.cs
+++ XPMMS/XPMMS/Pages/Team.xaml.cs
@@ -5,18 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+
 using XPMMS.Models;
 
 namespace XPMMS.Pages
 {
 	public partial class Team : ContentPage
 	{
-	    private UserModel loggedInUser;
+	    private UserModel _user;
+	    private UserModel[] _members;
+        private TeamModel _team;
+        private ProjectModel _project;
+        private TaskModel[] _tasks;
 
-		public Team (UserModel loggedInUser)
+        public Team (UserModel user, UserModel[] members, TeamModel team, ProjectModel project, TaskModel[] tasks)
 		{
 			InitializeComponent ();
-		    this.loggedInUser = loggedInUser;
+
+            _user = user;
+            _members = members;
+            _team = team;
+            _project = project;
+            _tasks = tasks;
+
 		    SetPage();
 		}
 
@@ -30,52 +41,23 @@ namespace XPMMS.Pages
                 FontSize = 20,
                 MinimumHeightRequest = 0
             };
-            Label headerDescription = new Label
-            {
-                Text = "Join an existing Team",
-                TextColor = Color.White,
-                FontSize = 20,
 
-                MinimumHeightRequest = 0
-            };
+            //Label headerDescription = new Label
+            //{
+            //    Text = "Join an existing Team",
+            //    TextColor = Color.White,
+            //    FontSize = 20,
+
+            //    MinimumHeightRequest = 0
+            //};
+	        string leader = null;
+	        for (int i = 0; i < _members.Length; i++)
+	        {
+	            if (_team.Team_Leader == _members[i].User_ID)
+	                leader = _members[i].First_Name + " " + _members[i].Last_Name;
+	        }
 
             Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
-
-            //BackgroundColor = Color.Black;
-
-            Picker picker = new Picker
-            {
-                Title = "PMME Navigation",
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.FillAndExpand
-            };
-
-            string[] navPages =
-            {
-                "Dashboard",
-                "About",
-                "Contact",
-                "Login",
-                "Register"
-            };
-
-            foreach (string navPage in navPages)
-            {
-                picker.Items.Add(navPage);
-            }
-
-            Grid nav = new Grid
-            {
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-
-
-            };
-
-            nav.Children.Add(new Button { Text = "XPMME" }, 0, 0);
-            nav.Children.Add(new Button { Text = "DashBoard" }, 1, 0);
-            nav.Children.Add(new Button { Text = "About" }, 2, 0);
-            nav.Children.Add(new Button { Text = "Contact" }, 3, 0);
 
             Grid inputGrid = new Grid
             {
@@ -84,22 +66,33 @@ namespace XPMMS.Pages
             };
 
             inputGrid.Children.Add(new Label { Text = "Team Name:" }, 0, 0);
-            inputGrid.Children.Add(new Label { Text = "Digital Anomaly" }, 1, 0);
+            inputGrid.Children.Add(new Label { Text = _team.Team_Name }, 1, 0);
             inputGrid.Children.Add(new Label { Text = "Team Leader:" }, 0, 1);
-            inputGrid.Children.Add(new Label { Text = "John Doe" }, 1, 1);
-            inputGrid.Children.Add(new Label { Text = "Member Name:" }, 0, 2);
-            inputGrid.Children.Add(new Label { Text = "James Young" }, 1, 2);
-            inputGrid.Children.Add(new Label { Text = "Member Name:" }, 0, 3);
-            inputGrid.Children.Add(new Label { Text = "Rica Page" }, 1, 3);
-            inputGrid.Children.Add(new Label { Text = "Member Name:" }, 0, 4);
-            inputGrid.Children.Add(new Label { Text = "David O'Reilly" }, 1, 4);
+            inputGrid.Children.Add(new Label { Text =  leader}, 1, 1);
+	        int count = 2;
+            foreach (var user in _members)
+	        {
+	            if (user.User_ID != _team.Team_Leader)
+	            {
+
+                    inputGrid.Children.Add(new Label { Text = "Member Name:" }, 0, count);
+	                inputGrid.Children.Add(new Label {Text = user.First_Name + " " + user.Last_Name}, 1, count);
+	                count++;
+	            }
+            }
+
+            //inputGrid.Children.Add(new Label { Text = "Member Name:" }, 0, 2);
+            //inputGrid.Children.Add(new Label { Text = "James Young" }, 1, 2);
+            //inputGrid.Children.Add(new Label { Text = "Member Name:" }, 0, 3);
+            //inputGrid.Children.Add(new Label { Text = "Rica Page" }, 1, 3);
+            //inputGrid.Children.Add(new Label { Text = "Member Name:" }, 0, 4);
+            //inputGrid.Children.Add(new Label { Text = "David O'Reilly" }, 1, 4);
 
 
             Content = new StackLayout
             {
                 Children =
                 {
-                    picker,
                     header,
                     inputGrid
                 }
