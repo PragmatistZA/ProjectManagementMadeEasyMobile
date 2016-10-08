2a65711ae63e99b418d674be0f10acfe0e0d1b3e
PragmatistZA <shaun.cilliers@gmail.com>
2016-10-08 13:11:16 +0200

    removed old code

--- XPMMS/XPMMS/Pages/Login.xaml.cs
+++ XPMMS/XPMMS/Pages/Login.xaml.cs
@@ -20,16 +20,12 @@ namespace XPMMS.Pages
         public Login()
         {
             InitializeComponent();
+
             SetPage();
         }
 
         private void SetPage()
 	    {
-            ToolbarItem btn1Item = new ToolbarItem
-            {
-                Text = "Test"
-            };
-
             Label header = new Label
             {
                 Text = "Log in",
@@ -37,52 +33,8 @@ namespace XPMMS.Pages
                 FontSize = 20,
                 MinimumHeightRequest = 0
             };
-            Label headerDescription = new Label
-            {
-                Text = "A mobile application tool designed to make projects much more bearable.",
-                TextColor = Color.White,
-                FontSize = 15,
-
-                MinimumHeightRequest = 0
-            };
 
             Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
-
-            //BackgroundColor = Color.Black;
-
-            Picker picker = new Picker
-            {
-                Title = "PMME Navigation",
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.FillAndExpand
-            };
-
-            string[] navPages =
-            {
-                "Dashboard",
-                "About",
-                "Contact",
-                "Login",
-                "Register"
-            };
-
-            foreach (string navPage in navPages)
-            {
-                picker.Items.Add(navPage);
-            }
-
-            Grid nav = new Grid
-            {
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-
-
-            };
-
-            nav.Children.Add(new Button { Text = "XPMME" }, 0, 0);
-            nav.Children.Add(new Button { Text = "DashBoard" }, 1, 0);
-            nav.Children.Add(new Button { Text = "About" }, 2, 0);
-            nav.Children.Add(new Button { Text = "Contact" }, 3, 0);
 
             Grid inputGrid = new Grid
             {
@@ -108,7 +60,6 @@ namespace XPMMS.Pages
             {
                 Children =
                 {
-                    picker,
                     header,
                     inputGrid
                 }
--- XPMMS/XPMMS/Pages/Register.xaml.cs
+++ XPMMS/XPMMS/Pages/Register.xaml.cs
@@ -22,10 +22,6 @@ namespace XPMMS.Pages
         }
         private void SetPage()
         {
-            ToolbarItem btn1Item = new ToolbarItem
-            {
-                Text = "Test"
-            };
             Label header = new Label
             {
                 Text = "Register",
@@ -33,47 +29,15 @@ namespace XPMMS.Pages
                 FontSize = 20,
                 MinimumHeightRequest = 0
             };
-            Label headerDescription = new Label
-            {
-                Text = "A mobile application tool designed to make projects much more bearable.",
-                TextColor = Color.White,
-                FontSize = 15,
-                MinimumHeightRequest = 0
-            };
+
             Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
-            //BackgroundColor = Color.Black;
-            Picker picker = new Picker
-            {
-                Title = "PMME Navigation",
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.FillAndExpand
-            };
-            string[] navPages =
-            {
-                "Dashboard",
-                "About",
-                "Contact",
-                "Login",
-                "Register"
-            };
-            foreach (string navPage in navPages)
-            {
-                picker.Items.Add(navPage);
-            }
-            Grid nav = new Grid
-            {
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-            };
-            nav.Children.Add(new Button { Text = "XPMME" }, 0, 0);
-            nav.Children.Add(new Button { Text = "DashBoard" }, 1, 0);
-            nav.Children.Add(new Button { Text = "About" }, 2, 0);
-            nav.Children.Add(new Button { Text = "Contact" }, 3, 0);
+
             Grid inputGrid = new Grid
             {
                 VerticalOptions = LayoutOptions.Start,
                 HorizontalOptions = LayoutOptions.FillAndExpand,
             };
+
             _btnRegister = new Button { Text = "Register" };
             _userName = new Editor { Text = "John" };
             _userSurname = new Editor { Text = "Doe" };
@@ -81,6 +45,7 @@ namespace XPMMS.Pages
             _userPassword = new Entry { Text = "Password", IsPassword = true };
             _userConfirmPassword = new Entry { Text = "Password", IsPassword = true };
             _btnRegister.Clicked += BtnRegister_Clicked;
+
             inputGrid.Children.Add(new Label { Text = "First Name:" }, 0, 0);
             inputGrid.Children.Add(_userName, 1, 0);
             inputGrid.Children.Add(new Label { Text = "Last Name:" }, 0, 1);
@@ -96,13 +61,13 @@ namespace XPMMS.Pages
             {
                 Children =
                 {
-                    picker,
                     header,
                     inputGrid
                 }
             };
         }
-        public async void BtnRegister_Clicked(object sender, EventArgs e)
+
+        private async void BtnRegister_Clicked(object sender, EventArgs e)
         {
             if (_userPassword.Text == _userConfirmPassword.Text)
             {
